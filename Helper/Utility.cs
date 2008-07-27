625c950aff5b49e3b50bc27bd4c0c9b1b24e9cb9
hazzik <hazzik@gmail.com>
2008-07-27 17:26:39 +0000

    git-svn-id: http://localhost:88/svn/uwow4/trunk/uwow2@2 3b753528-4e94-6941-993a-e4fe79dda6b7

--- Helper/Utility.cs
+++ Helper/Utility.cs
@@ -3,164 +3,130 @@ using System;
 using System.Reflection;
 using System.Text;
 using System.IO;
+using System.Collections.Generic;
 
-namespace Helper
-{
-	public static  class Utility
-	{
+namespace Helper {
+	public static class Utility {
 
-		private static DateTime deltaTime=  new DateTime(1970, 1, 1);
+		private static DateTime deltaTime = new DateTime(1970, 1, 1);
 
 		public static Random seed2 = new Random();
 
-		public static int Random(int max)
-		{
+		public static int Random(int max) {
 			return seed2.Next(max);
 		}
 
-		public static int Random(int min, int max)
-		{
-			if (min < max)
+		public static int Random(int min, int max) {
+			if(min < max)
 				return seed2.Next(min, max);
 			return min;
 		}
 
-		public static int Random4()
-		{
+		public static int Random4() {
 			return seed2.Next() & 0x00000003;
 		}
 
-		public static int Random8()
-		{
+		public static int Random8() {
 			return seed2.Next() & 0x00000007;
 		}
 
-		public static int Random16()
-		{
+		public static int Random16() {
 			return seed2.Next() & 0x0000000F;
 		}
 
-		public static int Random1024()
-		{
+		public static int Random1024() {
 			return seed2.Next() & 0x000003FF;
 		}
 
-		public static int Random1024x1024()
-		{
+		public static int Random1024x1024() {
 			return seed2.Next() & 0x000FFFFF;
 		}
 
-		public static double RandomDouble()
-		{
+		public static double RandomDouble() {
 			return seed2.NextDouble();
 		}
 
-		public static bool ToBoolean(string value)
-		{
+		public static bool ToBoolean(string value) {
 			bool val;
-			try
-			{
+			try {
 				val = Convert.ToBoolean(value);
-			}
-			catch
-			{
+			} catch {
 				val = false;
 			}
 			return val;
 		}
 
-		public static double ToDouble(string value)
-		{
+		public static double ToDouble(string value) {
 			double val;
-			try
-			{
+			try {
 				val = Convert.ToDouble(value);
-			}
-			catch
-			{
+			} catch {
 				val = 0D;
 			}
 			return val;
 		}
 
-		public static int ToInt32(string value)
-		{
+		public static int ToInt32(string value) {
 			int val;
-			try
-			{
-				if (value.StartsWith("0x"))
+			try {
+				if(value.StartsWith("0x"))
 					return Convert.ToInt32(value.Substring(2), 16);
 				else
 					val = Convert.ToInt32(value);
-			}
-			catch
-			{
+			} catch {
 				val = 0;
 			}
 			return val;
 		}
 
-		public static ulong ToUInt64(string value)
-		{
+		public static ulong ToUInt64(string value) {
 			ulong val;
-			try
-			{
-				if (value.StartsWith("0x"))
+			try {
+				if(value.StartsWith("0x"))
 					return Convert.ToUInt64(value.Substring(2), 16);
 				else
 					val = Convert.ToUInt64(value);
-			}
-			catch
-			{
+			} catch {
 				val = 0;
 			}
 			return val;
 		}
 
-		public static TimeSpan ToTimeSpan(string value)
-		{
+		public static TimeSpan ToTimeSpan(string value) {
 			TimeSpan val;
-			try
-			{
+			try {
 				val = TimeSpan.Parse(value);
-			}
-			catch
-			{
+			} catch {
 				val = TimeSpan.Zero;
 			}
 			return val;
 		}
 
-		public static uint ToUnixTimestamp(DateTime d)
-		{
+		public static uint ToUnixTimestamp(DateTime d) {
 			return (uint)(new TimeSpan(DateTime.Now.Ticks - deltaTime.Ticks)).TotalSeconds;
 		}
 
-		public static DateTime ToDateTime(uint unixTimestamp)
-		{
+		public static DateTime ToDateTime(uint unixTimestamp) {
 			return deltaTime.AddSeconds((double)unixTimestamp);
 		}
 
-		public static void View(TextWriter tw, byte[] b, int offset, int len)
-		{
+		public static void View(TextWriter tw, byte[] b, int offset, int len) {
 			int i1;
 			int i2;
 			int i3;
-			for (i1 = 0; (i1 < len); i1 += 16)
-			{
+			for(i1 = 0; (i1 < len); i1 += 16) {
 				tw.Write("{0:X10}: ", i1);
-				for (i2 = i1; ((i2 < (i1 + 16)) && ((i2 + offset) < b.Length)); i2++)
-				{
-					if (i2 >= len)
+				for(i2 = i1; ((i2 < (i1 + 16)) && ((i2 + offset) < b.Length)); i2++) {
+					if(i2 >= len)
 						tw.Write("-- ");
 					else
 						tw.Write("{0:X2} ", b[i2 + offset]);
-					if (i2 == i1 + 7)
+					if(i2 == i1 + 7)
 						tw.Write("| ");
 				}
 				tw.Write(" ");
-				for (i3 = i1; (((i3 + offset) < b.Length) && ((i3 < (i1 + 16)) && (i3 < len))); i3++)
-					if (b[i3 + offset] < 32 || b[i3 + offset] > 127)
+				for(i3 = i1; (((i3 + offset) < b.Length) && ((i3 < (i1 + 16)) && (i3 < len))); i3++)
+					if(b[i3 + offset] < 32 || b[i3 + offset] > 127)
 						tw.Write(".");
 					else
 						tw.Write("{0}", ((char)(b[i3 + offset])).ToString());
@@ -168,6 +134,7 @@ namespace Helper
 				tw.WriteLine();
 			}
 		}
+
 
 		public static byte[] Reverse(this byte[] from) {
 			byte[] res = new byte[from.Length];
@@ -184,5 +151,26 @@ namespace Helper
 			return tmp;
 		}
 
+		public static void WriteCString(this BinaryWriter w, string value) {
+			WriteCString(w, value, Encoding.UTF8);
+		}
+
+		public static void WriteCString(this BinaryWriter w, string value, Encoding encoding) {
+			w.Write(encoding.GetBytes(value));
+			w.Write((byte)0);
+		}
+
+		public static string ReadCString(this BinaryReader r) {
+			return ReadCString(r, Encoding.UTF8);
+		}
+
+		public static string ReadCString(this BinaryReader r, Encoding encoding) {
+			var b = (byte)0;
+			var buff = new List<byte>();
+			while((b = r.ReadByte()) != 0) {
+				buff.Add(b);
+			}
+			return encoding.GetString(buff.ToArray());
+		}
 	}
 }
