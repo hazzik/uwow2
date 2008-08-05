08ef74955832d5902b197629415a7f243b6118e8
hazzik <hazzik@gmail.com>
2008-08-05 11:00:04 +0000

    git-svn-id: http://localhost:88/svn/uwow4/trunk/uwow2@57 3b753528-4e94-6941-993a-e4fe79dda6b7

--- src/RealmServer/WorldObject.cs
+++ src/RealmServer/WorldObject.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Hazzik.RealmServer {
-	public class WorldObject {
+namespace Hazzik {
+	public abstract class WorldObject {
+		//OBJECT_FIELD_GUID = 0, // 2 4 1
+		public virtual long Guid { get; set; }
+		//OBJECT_FIELD_TYPE = 2, // 1 1 1
+		public virtual int Type { get; set; }
+		//OBJECT_FIELD_ENTRY = 3, // 1 1 1
+		public virtual int Entry { get; set; }
+		//OBJECT_FIELD_SCALE_X = 4, // 1 3 1
+		public virtual float ScaleX { get; set; }
+		//OBJECT_FIELD_PADDING = 5, // 1 1 0
+		public virtual int Pad { get { return 0; } }
 	}
 }
