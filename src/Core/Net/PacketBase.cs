33b07e030f57b21224913ded837b23431c6001b2
hazzik <hazzik@gmail.com>
2008-08-10 10:07:14 +0000

    git-svn-id: http://localhost:88/svn/uwow4/trunk/uwow2@65 3b753528-4e94-6941-993a-e4fe79dda6b7

---
+++ src/Core/Net/PacketBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Hazzik.Net {
+	public class PacketBase : IPacket {
+		private Stream _stream;
+
+		protected internal PacketBase(int code, byte[] data) {
+			_stream = new MemoryStream(data, false);
+			this.Code = code;
+		}
+
+		protected internal PacketBase(int code) {
+			this.Code = code;
+		}
+
+		public int Code { get; set; }
+
+		public int Size { get { return (int)_stream.Length; } }
+
+		public Stream GetStream() {
+			if(_stream == null) {
+				_stream = new MemoryStream();
+			}
+			return _stream;
+		}
+	}
+}
