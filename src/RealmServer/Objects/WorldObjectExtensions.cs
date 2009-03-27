f4978d09a3123d30f8537c7120f4888287f9c786
hazzik <hazzik@gmail.com>
2009-03-27 11:13:51 +0000

    * removed unused class
    git-svn-id: http://localhost:88/svn/uwow4/uwow2/trunk@285 3b753528-4e94-6941-993a-e4fe79dda6b7

--- src/RealmServer/Objects/WorldObjectExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Hazzik.Objects {
-	public static class WorldObjectExtensions {
-		public static ulong EntityId(this WorldObject source) {
-			return (null != source) ? source.Guid : 0;
-		}
-	}
-}
