5761f47e893b29feec682c20e952409b74ffbd3a
hazzik <hazzik@gmail.com>
2009-09-26 22:29:46 +0000

    save some stuff into db
    git-svn-id: http://localhost:88/svn/uwow4/uwow2/trunk@491 3b753528-4e94-6941-993a-e4fe79dda6b7

--- src/RealmServer.Data.NH.Fluent/Mappings/PlayerMap.cs
+++ src/RealmServer.Data.NH.Fluent/Mappings/PlayerMap.cs
@@ -9,6 +9,17 @@ namespace Hazzik.RealmServer.Data.NH.Fluent.Mappings {
 			Not.LazyLoad();
 			Id(p => p.Guid).Column("PlayerId").UnsavedValue("any").GeneratedBy.Native();
 			Map(p => p.Name).Column("Name").Not.Nullable().Unique().Length(255);
+			Map(p => p.Race).Column("Race").CustomType(typeof(Races));
+			Map(p => p.Classe).Column("Class").CustomType(typeof(Classes));
+			Map(p => p.Gender).Column("Gender").CustomType(typeof(GenderType));
+			Map(p => p.Skin).Column("Skin");
+			Map(p => p.Face).Column("Face");
+			Map(p => p.HairStyle).Column("HairStyle");
+			Map(p => p.HairColor).Column("HairColor");
+			Map(p => p.FacialHair).Column("FacialHair");
+			Map(p => p.Level).Column("Level");
+			Map(p => p.DisplayId).Column("DisplayId");
+			Map(p => p.NativeDisplayId).Column("NativeDisplayId");
 		}
 	}
 }
