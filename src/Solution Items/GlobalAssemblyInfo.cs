944a455f9d2b3262ffdab5b02602f062ce106d54
hazzik <hazzik@gmail.com>
2009-10-22 14:02:17 +0000

    GlobalAssemblyInfo.cs - CLSCompliant
    git-svn-id: http://localhost:88/svn/uwow4/uwow2/trunk@529 3b753528-4e94-6941-993a-e4fe79dda6b7

--- src/Solution Items/GlobalAssemblyInfo.cs
+++ src/Solution Items/GlobalAssemblyInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System;
 
 [assembly: AssemblyProduct("Hazzik Wow Server")]
 [assembly: AssemblyCopyright("Copyright © Hazzik 2006-2009")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
