be6711ab71fe07acec091b4fb678e37ba8c900e3
hazzik <hazzik@gmail.com>
2010-02-25 20:46:35 +0000

    updated copyright
    git-svn-id: http://localhost:88/svn/uwow4/uwow2/trunk@579 3b753528-4e94-6941-993a-e4fe79dda6b7

--- src/Solution Items/GlobalAssemblyInfo.cs
+++ src/Solution Items/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using System;
 
 [assembly: AssemblyProduct("Hazzik Wow Server")]
-[assembly: AssemblyCopyright("Copyright © Hazzik 2006-2009")]
+[assembly: AssemblyCopyright("Copyright © Hazzik 2006-2010")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
