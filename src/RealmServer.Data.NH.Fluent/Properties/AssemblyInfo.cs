513c200a822b23d15b524192caa1d1b73b54621f
hazzik <hazzik@gmail.com>
2011-04-26 01:15:33 +0600

    removed deprecated project

--- src/RealmServer.Data.NH.Fluent/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("RealmServer.Data.NH.Fluent")]
-[assembly: Guid("e815ce45-b2c5-4077-b064-8c5408655320")]
